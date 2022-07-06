72c29e4283f0ec1cd7bcc8eaee45fffc4b46f62f
SimJhaymez <chimobi.ndidiaha@gmail.com>
2022-07-06 01:49:32 -0700

    variables if else while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,34 +1,19 @@
 #include <stdio.h>
 
 /**
-  * main - Prints the numbers from 00 to 99
-  *
-  * Return: Always (Success)
-  */
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-        int c, i;
+	char c;
 
-        c = i = '0';
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
-        for (c = '0'; c <= '9'; c++)
-        {
-                for (i = '0'; i <= '9'; i++)
-                {
-                        putchar(c);
-                        putchar(i);
-
-                        if ((c != '9') || (c == '9' && i != '9'))
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-
-        putchar('\n');
-
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
 
